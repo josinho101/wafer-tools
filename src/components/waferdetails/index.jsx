39c34f1745bf2561b272812645c2906f61bf3eba
Joseph Johny Emmatty <josinho.seven@gmail.com>
2021-07-29 07:43:44 +0530

    Added logic to display wafer in details page

--- src/components/waferdetails/index.jsx
+++ src/components/waferdetails/index.jsx
@@ -1,14 +1,56 @@
 import clsx from "clsx";
+import Wafer from "../wafer";
 import { useStyles } from "./style";
-import { Paper, Grid } from "@material-ui/core";
+import { Paper, Grid, Box } from "@material-ui/core";
+import { useState, useEffect } from "react";
+import { generateDiesAndDefects } from "../../utils/waferhelper";
 
 const WaferDetails = () => {
   const classes = useStyles();
 
+  const scale = 1.5;
+  const waferRadius = 150;
+  const defectDiameter = 1.25;
+  const maxDefectsInDie = 2;
+  const canvasSize = waferRadius * 2 * scale;
+  const diePitch = {
+    height: 10,
+    width: 10,
+  };
+
+  const [dies, setDies] = useState([]);
+  const [defects, setDefects] = useState([]);
+  const [doReset, setDoReset] = useState(0);
+
+  useEffect(() => {
+    const [dies, defects] = generateDiesAndDefects(
+      canvasSize,
+      waferRadius,
+      defectDiameter,
+      maxDefectsInDie,
+      diePitch,
+      true
+    );
+    setDies(dies);
+    setDefects(defects);
+  }, [doReset]);
+
   return (
     <Grid container className={classes.container}>
       <Grid item xs={12}>
-        <Paper className={clsx(classes.paper, classes.waferPaper)}></Paper>
+        <Paper className={clsx(classes.paper, classes.waferPaper)}>
+          <Box>
+            <Wafer
+              scale={scale}
+              dies={dies}
+              diePitch={diePitch}
+              canvasSize={canvasSize}
+              waferRadius={waferRadius}
+              defectDiameter={defectDiameter}
+              defects={defects}
+            />
+          </Box>
+        </Paper>
       </Grid>
     </Grid>
   );
